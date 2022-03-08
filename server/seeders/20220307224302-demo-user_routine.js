2ec62b9d24251a544cb9dec514a3376b86e7645c
2cho6 <evenabee@gmail.com>
2022-03-08 15:21:34 +0900

    Feat : make personal routine functions

--- server/seeders/20220307224302-demo-user_routine.js
+++ server/seeders/20220307224302-demo-user_routine.js
@@ -7,7 +7,7 @@ module.exports = {
       [
         {
           list: JSON.stringify({ contents: ["물 2L 마시기", "스트레칭 하기"] }),
-          user_id: 1,
+          user_id: 3,
           user_cal_id: 1,
           daily_check: JSON.stringify({ checked: [0, 0] }),
           createdAt: new Date(),
@@ -18,6 +18,14 @@ module.exports = {
           user_id: 2,
           user_cal_id: 2,
           daily_check: JSON.stringify({ checked: [0, 0] }),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          list: JSON.stringify({ contents: ["스트레칭 하기"] }),
+          user_id: 3,
+          user_cal_id: 2,
+          daily_check: JSON.stringify({ checked: [0] }),
           createdAt: new Date(),
           updatedAt: new Date(),
         },
