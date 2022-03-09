936727d2059189ea4737bd96b8e294d4f5082ece
OverFlowBIN <87470206+OverFlowBIN@users.noreply.github.com>
2022-03-09 20:24:48 +0900

    Feat : group_routine_2

--- server/app/controllers/group_routines.js
+++ server/app/controllers/group_routines.js
@@ -40,31 +40,39 @@ module.exports = {
       // .then(result => res.json(result));
 
       const max = await group_routine.findOne({
-        order: [ [ 'id', 'DESC' ]],
+        order: [[ 'id', 'DESC' ]],
         })
-      res.json(max.id)
+      res.json({ data: { 
+        month: max.createdAt.getMonth() + 1, 
+        date: max.createdAt.getDate(),
+        day: max.createdAt.getDay()
+        },
+      })
+
+
     }
   },
 
+  // TODO: 그룹루틴 컴포넌트별로(그룹루틴 창에서) id를 클라에서 확인할수 있게 저장해두기 -> 이후 컴포넌트 클릭시 작동할 수 있게
   // 내가 가입한 그룹 루틴 보여주기
   group_routines: {
     get: async (req, res) => {
       
       // TODO: client 연결 후 이메일은 토큰으로 바꾸기
-      const { email } = await req.body;
+      // const { email } = await req.body;
 
-      // function getCookie(name) {
-      //   let matches = req.headers.cookie.match(
-      //     new RegExp(
-      //       "(?:^|; )" +
-      //         name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") +
-      //         "=([^;]*)"
-      //     )
-      //   );
-      //   return matches ? decodeURIComponent(matches[1]) : undefined;
-      // }
-      // const accessToken = getCookie("accessToken");
-      // const { email } = jwt.verify(accessToken, process.env.ACCESS_SECRET);
+      function getCookie(name) {
+        let matches = req.headers.cookie.match(
+          new RegExp(
+            "(?:^|; )" +
+              name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") +
+              "=([^;]*)"
+          )
+        );
+        return matches ? decodeURIComponent(matches[1]) : undefined;
+      }
+      const accessToken = getCookie("accessToken");
+      const { email } = jwt.verify(accessToken, process.env.ACCESS_SECRET);
 
       try {
         // 유저테이블에서 나의 user.id 찾기
@@ -100,20 +108,20 @@ module.exports = {
     get: async (req, res) => {
 
        // TODO: client 연결 후 이메일은 토큰으로 바꾸기
-       const { email } = await req.body;
+      //  const { email } = await req.body;
 
-       // function getCookie(name) {
-       //   let matches = req.headers.cookie.match(
-       //     new RegExp(
-       //       "(?:^|; )" +
-       //         name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") +
-       //         "=([^;]*)"
-       //     )
-       //   );
-       //   return matches ? decodeURIComponent(matches[1]) : undefined;
-       // }
-       // const accessToken = getCookie("accessToken");
-       // const { email } = jwt.verify(accessToken, process.env.ACCESS_SECRET);
+       function getCookie(name) {
+         let matches = req.headers.cookie.match(
+           new RegExp(
+             "(?:^|; )" +
+               name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") +
+               "=([^;]*)"
+           )
+         );
+         return matches ? decodeURIComponent(matches[1]) : undefined;
+       }
+       const accessToken = getCookie("accessToken");
+       const { email } = jwt.verify(accessToken, process.env.ACCESS_SECRET);
 
 
       try {
@@ -135,14 +143,15 @@ module.exports = {
         } 
         
         // 가입 안한 루틴 중 All tag가 아닌 다른 태그가 올때
-        return await group_routine.findAll({
+        const unRegisterdGroupByTag =  await group_routine.findAll({
             where: {
               [Op.and]: [
                 { tag_name: { [Op.substring]: req.query.name }},
                 { id : { [Op.notIn] : myGroupId }}
               ]
             }
-          }).then(data => res.status(200).json({data, message: "가입 안한 루틴 중 요청된 태그가 포함된 데이터"}));
+          })
+        return res.status(200).json({data: unRegisterdGroupByTag, message: "가입 안한 루틴 중 요청된 태그가 포함된 데이터"})
       }
       
       catch {
@@ -157,20 +166,20 @@ module.exports = {
       const {routine_name, tag_name, image, contents} = await req.body;
 
       // TODO: client 연결 후 이메일은 토큰으로 바꾸기
-      const { email } = await req.body;
+      // const { email } = await req.body;
 
-      // function getCookie(name) {
-      //   let matches = req.headers.cookie.match(
-      //     new RegExp(
-      //       "(?:^|; )" +
-      //         name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") +
-      //         "=([^;]*)"
-      //     )
-      //   );
-      //   return matches ? decodeURIComponent(matches[1]) : undefined;
-      // }
-      // const accessToken = getCookie("accessToken");
-      // const { email } = jwt.verify(accessToken, process.env.ACCESS_SECRET);
+      function getCookie(name) {
+        let matches = req.headers.cookie.match(
+          new RegExp(
+            "(?:^|; )" +
+              name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") +
+              "=([^;]*)"
+          )
+        );
+        return matches ? decodeURIComponent(matches[1]) : undefined;
+      }
+      const accessToken = getCookie("accessToken");
+      const { email } = jwt.verify(accessToken, process.env.ACCESS_SECRET);
 
 
       // TODO: 태그 어떻게 넣을지 상의해보기!!
@@ -203,11 +212,104 @@ module.exports = {
     // 가입된 그룹 루틴 선택 : 코멘트를 쓰는 인풋창 있음, 콘텐츠, 날짜선택, 댓글, 루틴달성률, 그룹탈퇴하기 버튼
     // 가입안된 그룹 루틴 선택 : 콘텐츠, 날짜선택, 댓글, 루틴달성률, 그룹가입하기 버튼
 
+
+// GET : localhost:4000/group-routine/select?id=3
   select_group_routine: {
-    get: (req, res) => {
+    get: async (req, res) => {
+      // 나의 토큰 이메일과, 그룹루틴 id를 이용하여 그룹루틴 클릭하기.
+
+      // TODO: client 연결 후 이메일은 토큰으로 바꾸기
+      // const { email } = await req.body;
+
+      function getCookie(name) {
+        let matches = req.headers.cookie.match(
+          new RegExp(
+            "(?:^|; )" +
+              name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") +
+              "=([^;]*)"
+          )
+        );
+        return matches ? decodeURIComponent(matches[1]) : undefined;
+      }
+      const accessToken = getCookie("accessToken");
+      const { email } = jwt.verify(accessToken, process.env.ACCESS_SECRET);
+
+      try {
+        // 그룹 루틴 아이디 받아오기
+        // localhost:4000/group-routine/select?id=2&date=1646715662218
+        const groupRoutineID = +req.query.id;
+        const date = +req.query.date;
+
+        // 유저테이블에서 나의 user.id 찾기
+        const myUserId = await user.findOne({where : { email }})
+          .then(data => data.id)
+
+        // 내가 가입한 그룹루틴 아이디 찾기 => [1, 3, 4, 5, 6, 8]
+        const myGroupId = await group_user.findAll({ where : { user_id : myUserId }})
+          .then(data => data.map(el => el.group_routine_id));
+
+
+        // 선택된 그룹 루틴 데이터
+        const selectedGroupRoutineData = await group_routine.findOne({ where : { id : groupRoutineID }})
+
+        // 선택된 달, 요일
+        const year = () => new Date(date).getFullYear();
+        const month = () => {
+          let result = new Date(date).getMonth() + 1;
+          if(result < 10) {
+            return result = '0' + result;
+          } return result;
+        };
+        const day = () => {
+          let result = new Date(date).getDate()
+          if(result < 10) {
+            return result = '0' + result;
+          } return result;
+        };
+ 
+        // xxxx-xx-xx 형식의 선택된 데이터값
+        const selectedDate = `${year()}-${month()}-${day()}`;
+        // console.log(selectedDate)
+
+        // 해당 날짜의 댓글 가져오기
+        const selectedComment = await comment.findAll({ where : {
+            [Op.and]: [
+              { group_routine_id: groupRoutineID },
+              { createdAt : { [Op.startsWith]: selectedDate }}
+            ]
+          }
+        })
+
+        // const selectedComment = await comment.findAll({ where : { group_routine_id: groupRoutineID.toString()}})
+        // console.log('selectedComment', selectedComment)
+
+        if(myGroupId.includes(groupRoutineID)) {
+          // 가입된 그룹 루틴 선택 : 코멘트를 쓰는 인풋창 있음, 콘텐츠, 날짜선택, 댓글, 루틴달성률, 그룹탈퇴하기 버튼
+          // TODO: 해당 날 달성률, 댓글작성자 이름 (comments : [{writer: overflowbin, commemt: '안녕하세요~~'}])
+          return res.status(200).json({ data: selectedGroupRoutineData, comments: selectedComment, registed : true, message: "가입한 그룹 루틴 데이터"})
+          // return res.json('응 가입한 그룹맞아')
+        } else {
+          // 가입안된 그룹 루틴 선택 : 콘텐츠, 날짜선택, 댓글, 루틴달성률, 그룹가입하기 버튼, 전체 멤버수
+          // TODO: 전체 멤버수, 해당 날 달성률, 댓글작성자 이름 (comments : [{writer: overflowbin, commemt: '안녕하세요~~'}])
+          return res.status(200).json({ data: selectedGroupRoutineData,  registed : false, message: "가입안한 그룹 루틴 데이터"})
+        }
+      }
+
+      catch {
+        return res.status(400).json({ message: "Bad request" });
+      }
+
+    }
+  },
+
+  // 댓글 남기기
+  write_comment: {
+    post: (req, res) => {
 
     }
   }
+
+
 
 
 }
