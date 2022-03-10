a57bea38e383973a0eb22bb2e0257694bbc4f8a0
elina <elinapark0818@gmail.com>
2022-03-11 03:51:05 +0900

    Feat: created About && Add gif files feature_client_About path:'/client'

--- client/src/page/About.js
+++ client/src/page/About.js
@@ -1,7 +1,11 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
-import FE_elina from "../assets/FE_elina.gif";
-import smile from "../assets/smile.png";
+// import Dance from "../assets/Dance.gif";
+import YOF from "../assets/YOF.gif";
+import yb from "../assets/yb.gif";
+import green from "../assets/green.gif";
+import bible from "../assets/bible.gif";
+import elina from "../assets/elina.gif";
 
 const AboutBody = styled.div`
   display: flex;
@@ -9,6 +13,7 @@ const AboutBody = styled.div`
   justify-content: center;
   align-items: center;
   width: 100%;
+  margin-bottom: 10em;
 `;
 
 const AboutContainer = styled.div`
@@ -61,16 +66,65 @@ const Color = styled.h1`
   display: flex;
   justify-content: center;
   margin-top: 1em;
+  margin-bottom: 3em;
 `;
 
-const GifDiv = styled.div`
+const TextDiv = styled.div`
   display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  margin: 5em 0;
-  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.07), 0 2px 4px rgba(0, 0, 0, 0.07),
-    0 4px 8px rgba(0, 0, 0, 0.07), 0 8px 16px rgba(0, 0, 0, 0.07),
-    0 16px 32px rgba(0, 0, 0, 0.07), 0 32px 64px rgba(0, 0, 0, 0.07);
+  flex-direction: column;
+  width: 20em;
+  margin-left: 5em;
+
+  & h1 {
+    margin-bottom: 0.5em;
+  }
+`;
+
+const PositionDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 3em;
+  color: #697f6e;
+  margin-bottom: 10em;
+
+  & ul {
+    list-style: none;
+    margin: 1em 0;
+  }
+
+  & h1 {
+    font-weight: bold;
+  }
+`;
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 5em;
+`;
+
+const Team = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-right: 1em;
+  margin-bottom: 10em;
+  color: #697f6e;
+`;
+
+const ImgDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 3em;
+  transition: all 0.5s ease;
+
+  &:hover {
+    transform: scale(1.02);
+  }
 `;
 
 function About() {
@@ -80,114 +134,108 @@ function About() {
         <AboutDiv>
           <Color> ABOUT US</Color>
 
-          <GifDiv>
-            <img alt="elina" src={FE_elina} style={{ marginRight: "0.5em" }} />
-            <img alt="elina" src={FE_elina} style={{ marginRight: "0.5em" }} />
-            <img alt="elina" src={FE_elina} style={{ marginRight: "0.5em" }} />
-            <img alt="elina" src={FE_elina} />
-          </GifDiv>
+          <Team>
+            <img alt="YOF" src={YOF} />
 
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              paddingRight: "1em",
-              margin: "5em 0",
-            }}
-          >
-            <div style={{ paddingRight: "5em" }}>
-              <img alt="smile" src={smile} style={{ width: "20em" }} />
-            </div>
-            <div>
+            <TextDiv>
               <h1>Team : YOF</h1>
               <h3>
-                " 함께 만들어가는 스마트한 생활 <br />
-                하나씩 달성해나가는 좋은 습관 <br />
+                " 함께 만들어가는 스마트한 생활 하나씩 달성해나가는 좋은 습관
                 모두의 루틴으로 하루를 완성해보세요."
               </h3>
-            </div>
-          </div>
+            </TextDiv>
+          </Team>
 
-          <div style={{ margin: "3em" }}>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                paddingRight: "1em",
-              }}
+          <PositionDiv>
+            <Div
+              data-aos="fade-right"
+              data-aos-easing="ease-out-cubic"
+              data-aos-duration="1000"
             >
-              <div style={{ paddingRight: "5em" }}>
-                <img alt="smile" src={smile} style={{ width: "10em" }} />
-              </div>
-              <div>
-                <h1>김영빈</h1>
-                <h3>Position: Back-End</h3>
-                <h3>꽃보다 아름다운 우리의 팀장님</h3>
-              </div>
-            </div>
-          </div>
+              <ImgDiv>
+                <img alt="yb" src={yb} style={{ width: "15em" }} />
+              </ImgDiv>
 
-          <div style={{ margin: "3em" }}>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                paddingRight: "1em",
-              }}
-            >
-              <div style={{ paddingRight: "5em" }}>
-                <img alt="smile" src={smile} style={{ width: "10em" }} />
-              </div>
-              <div>
-                <h1>김영빈</h1>
-                <h3>Position: Back-End</h3>
-                <h3>꽃보다 아름다운 우리의 팀장님</h3>
-              </div>
-            </div>
-          </div>
+              <TextDiv>
+                <h1>김영빈 (Back-End)</h1>
+                <h3>👑 꽃보다 아름다운 YOF의 팀장님 </h3>
+                <ul>
+                  <li>데이터베이스 초기 설정 및 유지/보수</li>
+                  <li>그룹 루틴 CRUD(router, controller)</li>
+                  <li>그룹 루틴 사용자에 대한 응답 제어</li>
+                  <li>데이터베이스 관계 설정</li>
+                </ul>
+              </TextDiv>
+            </Div>
 
-          <div style={{ margin: "3em" }}>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                paddingRight: "1em",
-              }}
+            <Div
+              data-aos="fade-right"
+              data-aos-easing="ease-out-cubic"
+              data-aos-duration="1000"
             >
-              <div style={{ paddingRight: "5em" }}>
-                <img alt="smile" src={smile} style={{ width: "10em" }} />
-              </div>
-              <div>
-                <h1>김영빈</h1>
-                <h3>Position: Back-End</h3>
-                <h3>꽃보다 아름다운 우리의 팀장님</h3>
-              </div>
-            </div>
-          </div>
+              <ImgDiv>
+                <img alt="green" src={green} style={{ width: "15em" }} />
+              </ImgDiv>
 
-          <div style={{ margin: "3em" }}>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                paddingRight: "1em",
-              }}
+              <TextDiv>
+                <h1>이초록 (Back-End)</h1>
+                <h3>🧚 YOF의 엔딩요정</h3>
+                <ul>
+                  <li>서버 초기 설정</li>
+                  <li>개인루틴 CRUD(router, controller)</li>
+                  <li>정적 웹 호스팅 및 클라우드 프록시 서버 연결</li>
+                  <li>배포 및 도메인 연결</li>
+                </ul>
+              </TextDiv>
+            </Div>
+          </PositionDiv>
+
+          <PositionDiv>
+            <Div
+              data-aos="fade-right"
+              data-aos-easing="ease-out-cubic"
+              data-aos-duration="1000"
+              data-aos-delay="500"
             >
-              <div style={{ paddingRight: "5em" }}>
-                <img alt="smile" src={smile} style={{ width: "10em" }} />
-              </div>
-              <div>
-                <h1>김영빈</h1>
-                <h3>Position: Back-End</h3>
-                <h3>꽃보다 아름다운 우리의 팀장님</h3>
-              </div>
-            </div>
-          </div>
+              <ImgDiv>
+                <img alt="bible" src={bible} style={{ width: "15em" }} />
+              </ImgDiv>
+
+              <TextDiv>
+                <h1>박성경 (Front-End)</h1>
+                <h3>💯 YOF의 정신적 지주. 만능 치트키</h3>
+                <ul>
+                  <li>전체적인 UI/UX 디자인</li>
+                  <li>개인 루틴 & 그룹 루틴 기능</li>
+                  <li>회원가입 기능</li>
+                  <li>로그인 기능</li>
+                </ul>
+              </TextDiv>
+            </Div>
+
+            <Div
+              data-aos="fade-right"
+              data-aos-easing="ease-out-cubic"
+              data-aos-duration="1000"
+              data-aos-delay="500"
+            >
+              <ImgDiv>
+                <img alt="elina" src={elina} style={{ width: "15em" }} />
+              </ImgDiv>
+
+              <TextDiv>
+                <h1>박윤정 (Front-End)</h1>
+                <h3>🥰 YOF의 해피바이러스 </h3>
+                <ul>
+                  <li></li>
+                  <li>프로젝트 소개 페이지</li>
+                  <li>팀 소개 페이지</li>
+                  <li>회원정보 수정 기능</li>
+                  <li>회원탈퇴 기능</li>
+                </ul>
+              </TextDiv>
+            </Div>
+          </PositionDiv>
         </AboutDiv>
       </AboutContainer>
     </AboutBody>
